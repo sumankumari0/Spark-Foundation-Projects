12116c253fbab2bbc0bf9c6a3b6cf6ee3574d1b5
swarnimswaraj <68789632+swarnimswaraj@users.noreply.github.com>
2021-10-02 17:43:32 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<div class="jumbotron w3-display-right">
+  <h1>Loan Prediction 
+  Using Machine Learning </h1>
+  <p>fjgfgkjhggh</p>
+</div>
+
+  
+<div class="container">
+  <div class="row">
+    <div class="col-sm-4">
+      <h3>Column 1</h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
+      <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
+    </div>
+    <div class="col-sm-4">
+      <h3>Column 2</h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
+      <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
+    </div>
+    <div class="col-sm-4">
+      <h3>Column 3</h3>        
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
+      <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
+    </div>
+  </div>
+</div>
+<div class="container">
+  <div class="col-xs-12 col-md-12 col-lg-12">
+    <ul class="list-unstyled">
+	  <li>
+	    <div class="sts-icons">
+		  <div class="hex-img"> 
+		    <img src="app/assets/images/feature-icons/hexagon.svg" alt="Design 5" title="Design 5"> 
+			  <span class="hex-icon">
+			    <img src="app/assets/images/feature-icons/pan-card.png" alt="Documents for Home Credit Loan" title="Documents for Home Credit Loan">
+		      </span> 
+	      </div>			  
+		</div>
+		<div class="sts-text">Proof of Identity<br><strong>PAN Card</strong></div>
+	  </li>
+	  <li>
+	    <div class="sts-icons">
+		  <div class="hex-img">
+		    <img src="app/assets/images/feature-icons/hexagon.svg" alt="Design 1" title="Design 1"> 
+			  <span class="hex-icon">
+			    <img src="app/assets/images/feature-icons/address-proof.png" alt="Documents for Instant Loan" title="Documents for Instant Loan">
+			  </span>
+		  </div>
+		</div>
+		<div class="sts-text">Residential Address Proof<br><strong>Indian Citizenship</strong>
+		</div>
+	  </li>
+	  <li>
+	    <div class="sts-icons">
+		  <div class="hex-img"> 
+		    <img src="https://img.icons8.com/color/48/000000/hexagon.png" alt="Design 2" title="Design 2"> 
+			  <span class="hex-icon">
+			    <img src="loan_project/img/netbanking.png" alt="Mandatory Requirement for Loan" title="Eligibility for Home Credit Instant Loan">
+			  </span>
+		  </div>
+		</div>
+		<div class="sts-text">NetBanking<br><strong>Must Required</strong></div>
+	  </li>
+	  <li><div class="sts-icons"><div class="hex-img"><img src="app/assets/images/feature-icons/hexagon.svg" alt="Design 3" title="Design 3"> <span class="hex-icon"><img src="app/assets/images/feature-icons/age.png" alt="Eligibility for HCIN Loan" title="Eligibility for HCIN Loan"></span></div></div><div class="sts-text">Age<br><strong>Must be above 19 years of Age</strong></div></li>
+	  <li><div class="sts-icons"><div class="hex-img"><img src="app/assets/images/feature-icons/hexagon.svg" alt="Design 4" title="Design 4"> <span class="hex-icon"><img src="app/assets/images/feature-icons/income.png" alt="HCIN Eligibility Criteria" title="HCIN Eligibility Criteria"></span></div></div><div class="sts-text">Income<br><strong>Minimum ₹10000</strong></div></li>
+	</ul>
+  </div>
+</div>
+<img src="loan_project/img/netbanking.png" alt="Mandatory Requirement for Loan" title="Eligibility for Home Credit Instant Loan">
+<img src="loan_project\img\netbanking.png" alt="Girl in a jacket" width="500" height="600">
+</body>
+</html>
